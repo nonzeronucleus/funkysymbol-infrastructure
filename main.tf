80be08623cd510d6c0fd798b8e30306d114df4fd
Ian <nonzeronucleus@ianplumb.org>
2022-03-19 12:06:09 +0000

    Added credentials

--- main.tf
+++ main.tf
@@ -6,11 +6,34 @@ terraform {
     }
   }
   required_version = "> 0.14"
-}
+
+
 
 provider "aws" {
   region = var.aws_region
 }
+
+resource "aws_iam_user" "circleci" {
+  name = var.user
+  path = "/system/"
+}
+
+resource "aws_iam_access_key" "circleci" {
+  user = aws_iam_user.circleci.name
+}
+
+data "template_file" "circleci_policy" {
+  template = file("circleci_s3_access.tpl.json")
+  vars = {
+    s3_bucket_arn = aws_s3_bucket.app.arn
+  }
+}
+
+resource "local_file" "circle_credentials" {
+  filename = "tmp/circleci_credentials"
+  content  = "${aws_iam_access_key.circleci.id}\n${aws_iam_access_key.circleci.secret}"
+}
+
 
 data "aws_acm_certificate" "cert" {
   domain = "*.${var.domain}"
