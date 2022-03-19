c01ba8792e8cf6b415e4cc06d39a1596aeda53e4
Ian <nonzeronucleus@ianplumb.org>
2022-03-19 07:27:41 +0000

    init

---
+++ terraform/modules/variables.tf
@@ -0,0 +1,12 @@
+variable "aws_region" {
+  default     = "us-east-1"
+}
+
+variable "domain" {
+  default = "nonzeronucleus.com"
+}
+
+variable "subdomain" {
+  default = "covid-data"
+  # default = "tftest2"
+}
