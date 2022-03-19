55386813954a3aa66e007f7fcdcb9118535d9e48
Ian <nonzeronucleus@ianplumb.org>
2022-03-19 10:40:56 +0000

    Removes period froms bucket name

--- terraform/modules/main.tf
+++ terraform/modules/main.tf
@@ -10,7 +10,7 @@ data "aws_acm_certificate" "cert" {
 # Note: The bucket name needs to carry the same name as the domain!
 # http://stackoverflow.com/a/5048129/2966951
 resource "aws_s3_bucket" "site" {
-  bucket = "${var.subdomain}.${var.domain}"
+  bucket = "${var.subdomain}_${var.domain}"
 
   tags = {
     site = "${var.subdomain}.${var.domain}"
@@ -29,7 +29,7 @@ resource "aws_s3_bucket_policy" "site" {
             "Effect": "Allow",
             "Principal": "*",
             "Action": "s3:GetObject",
-            "Resource":["arn:aws:s3:::${var.subdomain}.${var.domain}/*"]
+            "Resource":["arn:aws:s3:::${var.subdomain}_${var.domain}/*"]
         }
     ]
 }
